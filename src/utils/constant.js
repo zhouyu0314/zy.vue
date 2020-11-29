d53d15423df664fbf365032fd7c79a5a0a6228e6
zhouyu0314 <181332969@qq.com>
2020-11-29 20:50:34 +0800

    前端可在线显示txt文档 对于office 还处于开发阶段

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,8 @@
 
-global._basePath = '/F:/';
+global._basePath = '/D:/';
 global._baseUrl = 'localhost';
 global._basePort = '8001';
 global._wsPort = '34123';
+global._base64 = require('js-base64').Base64;
 
 
